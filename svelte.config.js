c2233910db33af78a22d91d0a09af207fe8357fb
Pavalean-Alexandru <alexandru.pavalean@istitutoagnelli.it>
2026-05-18 11:15:34 +0200

    fix base path

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,9 @@ import adapter from '@sveltejs/adapter-static';
 
 const config = {
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: '/portfolio'
-    }
+    adapter: adapter({
+      fallback: '404.html'
+    })
   }
 };
 
